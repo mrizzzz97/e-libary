f1fe22e891004ec0439569022acce71dff3db85f
mrizzzz97 <rizkisuryapratama2020@gmail.com>
2026-02-20 10:36:20 +0700

    seeder and factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            CategorySeeder::class,
         ]);
+
+        \App\Models\Author::factory(8)->create();
+        \App\Models\Book::factory(25)->create();
     }
 }
